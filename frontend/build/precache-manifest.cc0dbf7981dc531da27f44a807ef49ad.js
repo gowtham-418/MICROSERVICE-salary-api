806b3b867f4e67bffde2aea20d3a05015d271382
KaifShakeel76 <kaifshakeel555@gmail.com>
2024-05-22 11:54:23 +0530

    Frontend reviewed and replaced

---
+++ frontend/build/precache-manifest.cc0dbf7981dc531da27f44a807ef49ad.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "85f5fc4dc786b2dfdb6f",
+    "url": "/static/css/main.249db24a.chunk.css"
+  },
+  {
+    "revision": "85f5fc4dc786b2dfdb6f",
+    "url": "/static/js/main.4d33ba86.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e009cb1e6253cc907303",
+    "url": "/static/css/2.6e625160.chunk.css"
+  },
+  {
+    "revision": "e009cb1e6253cc907303",
+    "url": "/static/js/2.1d958c92.chunk.js"
+  },
+  {
+    "revision": "f24b133ed4406bb17876c3ed7fa86f22",
+    "url": "/static/media/tabler-webfont.f24b133e.eot"
+  },
+  {
+    "revision": "b56d2ee6c65e0104e4ef76c027587e46",
+    "url": "/static/media/tabler-webfont.b56d2ee6.woff2"
+  },
+  {
+    "revision": "924bfe321562eb8cdedf675ad0e011b3",
+    "url": "/static/media/tabler-webfont.924bfe32.woff"
+  },
+  {
+    "revision": "68520a6b16e40c7b3538bae7c43eb776",
+    "url": "/static/media/tabler-webfont.68520a6b.ttf"
+  },
+  {
+    "revision": "ad55e63e508f5b0e55e9d2394a2732c7",
+    "url": "/static/media/tabler-webfont.ad55e63e.svg"
+  },
+  {
+    "revision": "f5afb1a8985b3d7c5ddce5e7a6496072",
+    "url": "/index.html"
+  }
+];
